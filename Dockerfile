6c3be6d3a2a7d8edcab5e8297ec98a26b71f47e9
Huy Tran <kingbazoka@gmail.com>
2020-04-14 00:07:52 -0700

    Multi language support

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 FROM node:10
 RUN apt-get update
 RUN apt-get install -y --no-install-recommends \
+    ca-certificates \
     build-essential \
     g++ \
-    libboost-all-dev
+    libboost-all-dev \
+    libc6-dev \
+    wget
+RUN wget https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init; \
+    chmod +x rustup-init; \
+    ./rustup-init -y --default-toolchain stable; \
+    rm rustup-init; \
+    ln -s $HOME/.cargo/bin/rustc /usr/local/bin/rustc; \
+    mkdir -p /playground;
 WORKDIR /
 COPY package*.json ./
 RUN npm install
--- index.js
+++ index.js
@@ -3,18 +3,37 @@ const fs = require('fs');
 const app = express();
 const exec = require('child_process').exec;
 const bodyParser = require('body-parser');
+const timeout = require('connect-timeout');
 
+app.use(timeout('60s'));
 app.use(bodyParser());
 
 app.use(express.static('public'));
 
 app.post('/execute', (req, res) => {
+  const root = "/playground";
   const code = req.body.code;
   const stdin = req.body.stdin;
-  const cmd = 'g++ main.cpp -std=c++11 -o main && ' + (stdin.length ? ' cat stdin.inp | ./main' : './main');
-  fs.writeFileSync('main.cpp', code);
-  fs.writeFileSync('stdin.inp', stdin);
-  exec(cmd, (err, stdout, stderr) => {
+  const lang = req.body.lang;
+  let cmd = '';
+  switch (lang) {
+    case 'cpp':
+      fs.writeFileSync(root + '/main.cpp', code);
+      cmd += 'g++ main.cpp -std=c++11 -o maincpp && ' + (stdin.length ? ' cat stdin.inp | ./maincpp' : './maincpp');
+      break;
+    case 'python':
+      fs.writeFileSync(root + '/main.py', code);
+      cmd += stdin.length ? 'cat stdin.inp | python main.py' : 'python main.py';
+      break;
+    case 'rust':
+      fs.writeFileSync(root + '/main.rs', code);
+      cmd += 'rustc main.rs -o mainrs && ' + (stdin.length ? 'cat stdin.inp | ./mainrs' : './mainrs');
+      break;
+    default:
+      break;
+  }
+  fs.writeFileSync(root + '/stdin.inp', stdin);
+  exec(cmd, { cwd: root }, (err, stdout, stderr) => {
     res.json({ err, stdout, stderr });
   });
 });
