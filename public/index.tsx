88e4be28973c20d3ef757fb79a579aa2787448c0
Huy Tran <kingbazoka@gmail.com>
2021-06-11 17:45:56 -0700

    Support multiple files

--- public/index.tsx
+++ public/index.tsx
@@ -11,9 +11,8 @@ import 'codemirror/mode/python/python.js';
 import 'codemirror/mode/go/go.js';
 import 'codemirror/mode/rust/rust.js';
 import 'codemirror/keymap/vim.js';
-
-// Config Vim key binding
-CodeMirror.Vim.map("jk", "<Esc>", "insert")
+import 'codemirror/addon/dialog/dialog.js';
+import 'codemirror/addon/dialog/dialog.css';
 
 const supportedLanguages = [
   {
@@ -48,92 +47,319 @@ const supportedLanguages = [
   }
 ];
 
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = React.useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+const useCodeEditor = (ref, keyMap = {}, options = {}, commands = []) => {
+  const cm = React.useRef();
+
+  React.useEffect(() => {
+    const Vim = (CodeMirror as any).Vim;
+    // Config Vim key binding
+    Vim.map("jk", "<Esc>", "insert")
+
+    for (let cmd of commands) {
+      const {command, short, fn} = cmd;
+      Vim.defineEx(command, short, fn);
+    }
+
+    cm?.current = CodeMirror(ref?.current, {
+      lineNumbers: true,
+      keyMap: 'vim',
+      theme: 'material-darker',
+      showCursorWhenSelecting: true,
+      ...options
+    });
+    cm?.current.addKeyMap(keyMap);
+  }, [ref]);
+
+  return cm;
+};
+
+const useFileSystem = (id) => {
+  const fs = React.useRef();
+  const [language, setLanguage] = useLocalStorage("__cpp-pad-saved-language", "cpp");
+  const [fileSystem, setFileSystem] = useLocalStorage(id, {
+    currentFileIndex: -1,
+    files: [
+      {
+        name: 'demo-01.js',
+        content: 'console.log("Hey you!")',
+        language: 'node',
+      },
+      {
+        name: 'demo-02.rs',
+        content: 'fn main() {\n  println!("OK This is a test");\n}',
+        language: 'rust'
+      },
+    ]
+  });
+
+  const all = () => {
+    return fileSystem.files;
+  };
+
+  const getCurrent = () => {
+    if (fileSystem.currentFileIndex !== -1) {
+      return fileSystem.files[fileSystem.currentFileIndex];
+    }
+  };
+
+  const get = (index) => {
+    return fileSystem.files[index];
+  };
+
+  const findIndex = (name) => {
+    return fileSystem.files.findIndex(file => file.name === name);
+  };
+
+  const newFile = () => {
+    window.location.href = "/";
+  };
+
+  const rename = (index, newName) => {
+        const files = fileSystem.files;
+        files[index].name = newName;
+        setFileSystem({
+          fileSystem,
+          files: files
+        });
+  };
+
+  const write = (index, name, content) => {
+    const files = fileSystem.files;
+    let writtenIndex = index;
+    if (files[index]) {
+      // edit file
+      files[index] = { name, content, language };
+    } else {
+      // new file
+      const nlen = files.push({ name, content, language });
+      writtenIndex = nlen - 1;
+    }
+    setFileSystem({
+      currentFileIndex: writtenIndex,
+      files: files
+    });
+  };
+
+  const setCurrentIndex = (index) => {
+    if (index < 0 || index > fileSystem.files.length) index = -1;
+    setFileSystem({
+      ...fileSystem,
+      currentFileIndex: index
+    });
+  };
+
+  const currentIndex = () => {
+    return fileSystem.currentFileIndex;
+  }
+
+  fs?.current = {
+    all, getCurrent, get, new: newFile, rename, write, setCurrentIndex, findIndex, currentIndex, language, setLanguage
+  };
+
+  return fs;
+};
+
+const FileItem = ({ index, selected, fileName, onRename }) => {
+  const contentRef = React.useRef();
+  const [editMode, setEditMode] = React.useState(false);
+  const keyPressHandler = (e) => {
+    if (e.key === "Enter") {
+      setEditMode(false);
+      if (onRename) {
+        onRename(index, contentRef.current.textContent);
+      }
+    }
+  };
+
+  const doubleClickHandler = () => {
+    setEditMode(true);
+    setTimeout(() => {
+      document.execCommand('selectAll',false,null);
+    }, 0);
+  };
+
+  return (
+    <a
+      className={`file-entry ${selected && "active"}`}
+      href={`#file=${fileName}`}
+      ref={contentRef}
+      contentEditable={editMode}
+      onDoubleClick={doubleClickHandler}
+      onKeyPress={keyPressHandler}
+    >{fileName}</a>
+  );
+};
+
 const App = () => {
   const editorRef = React.useRef();
+  const languageRef = React.useRef(null);
+  const languageChangeHandler = () => {
+    const lang = languageRef.current.value;
+    FileManager.current.setLanguage(lang);
+  };
   const [waiting, setWaiting] = React.useState(false);
   const [executionOutput, setExecutionOutput] = React.useState({
     stdout: '',
     stderr: ''
   });
+  const executeCode = (editor) => {
+    setWaiting(true);
+    const code = editor.getValue();
+    fetch('/execute', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: code,
+        lang: FileManager.current.language
+      })
+    })
+      .then(r => r.json())
+      .then(output => {
+        setWaiting(false);
+        setExecutionOutput({
+          stdout: output.stdout,
+          stderr: output.stderr
+        });
+      })
+      .catch(() => {
+        setWaiting(false);
+        setExecutionOutput({
+          stdout: 'No output',
+          stderr: 'Code execution failed'
+        });
+      });
+  };
+
+  const codeEditor = useCodeEditor(editorRef, {
+    "Cmd-Enter": executeCode,
+    "Ctrl-Enter": executeCode
+  }, null, [
+    {
+      command: "new",
+      short: "new",
+      fn: () => {
+        FileManager.current.new();
+      }
+    },
+    {
+      command: "write",
+      short: "w",
+      fn: (editor, { args = [] }) => {
+        const fileName = args[0] || FileManager.current.getCurrent()?.name;
+        if (fileName) {
+          const index = FileManager.current.findIndex(fileName);
+          FileManager.current.write(index, fileName, editor.getValue());
+        } else {
+          editor.openNotification(
+            "Please specify a valid file name to write",
+            {
+              bottom: true,
+              duration: 5000
+            }
+          );
+        }
+      },
+    }
+  ]);
+
+  const FileManager = useFileSystem("__cpp-pad-file-system");
 
   React.useEffect(() => {
-    let language = window.localStorage.getItem('__cpppad-saved-language') || 'cpp';
+    const found = supportedLanguages.find(l => l.lang === FileManager.current.language);
+    if (found) {
+      if (FileManager.current.currentIndex() === -1) {
+        codeEditor?.current?.setValue(found.template);
+      }
+      codeEditor?.current?.setOption('mode', found.cmMode);
+    }
+  }, [FileManager.current.language]);
 
-    const cm = CodeMirror(editorRef.current, {
-      mode: supportedLanguages[0].cmMode,
-      lineNumbers: true,
-      keyMap: 'vim',
-      theme: 'material-darker',
-      cursorBlinkRate: 0,
-      showCursorWhenSelecting: true
-    });
+  const locationHashChangeHandler = () => {
+    const { hash } = window.location;
+    const fileName = hash.match(/#file=(.*)/)?.pop();
+    const index = FileManager.current.findIndex(fileName);
+    FileManager.current.setCurrentIndex(index);
+    if (index !== -1) {
+      const { content, language } = FileManager.current.get(index);
+      codeEditor?.current?.setValue(content);
+      FileManager.current.setLanguage(language);
+    }
+  };
 
-    const initLanguage = lang => {
-      const found = supportedLanguages.find(l => l.lang === lang);
-      language = lang;
-      window.localStorage.setItem('__cpppad-saved-language', found.lang);
-      const savedCode = window.localStorage.getItem('__cpppad-saved-code-' + found.lang) || found.template;
-      cm.setValue(savedCode);
-      cm.setOption('mode', found.cmMode);
-    };
-
-    initLanguage(language);
-
-    const $languageSelect = document.querySelector("#language-select");
-    $languageSelect.value = language;
-    $languageSelect.onchange = (e) => {
-      initLanguage($languageSelect.value);
-    };
-
-    const executeCode = (editor) => {
-      setWaiting(true);
-      const code = editor.getValue();
-      window.localStorage.setItem('__cpppad-saved-code-' + language, code);
-      fetch('/execute', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          stdin: '',
-          code: code,
-          lang: language
-        })
-      })
-        .then(r => r.json())
-        .then(output => {
-          setWaiting(false);
-          setExecutionOutput({
-            stdout: output.stdout,
-            stderr: output.stderr
-          });
-        })
-        .catch(() => {
-          setWaiting(false);
-          setExecutionOutput({
-            stdout: 'No output',
-            stderr: 'Code execution failed'
-          });
-        });
-    };
-
-    const customKey = {
-      "Cmd-Enter": executeCode,
-      "Ctrl-Enter": executeCode
-    };
-
-    cm.addKeyMap(customKey);
-
+  React.useEffect(() => {
+    locationHashChangeHandler();
+    window.addEventListener("hashchange", locationHashChangeHandler, false);
+    return () => {
+      window.removeEventListener("hashchange", locationHashChangeHandler, false);
+    }
   }, []);
 
   return (
     <>
       <div className="header">
-        language: <select id="language-select">
-          {supportedLanguages.map(({ lang, name }) => <option value={lang}>{name}</option>)}
+        language:&nbsp;
+        <select
+          id="language-select"
+          ref={languageRef}
+          onChange={languageChangeHandler}
+          value={FileManager.current.language}
+        >
+          {supportedLanguages.map(({ lang, name }) => <option key={lang} value={lang}>{name}</option>)}
         </select>
       </div>
       <div className="container">
+        <div id="file-tree">
+          <div className="file-controller">
+            <button onClick={FileManager?.current.new}>New File</button>
+          </div>
+          {FileManager.current.all().map((file, index) => (
+            <FileItem
+              key={index}
+              index={index}
+              selected={index === FileManager.current.currentIndex()}
+              fileName={file.name}
+              onRename={FileManager?.current.rename}
+            />
+          ))}
+        </div>
         <div id="editor-container">
           <div id="editor" ref={editorRef}></div>
         </div>
